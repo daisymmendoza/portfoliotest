423c6c623f6f2783167bb7978f231cfb83766de8
daisymmendoza <89948539+daisymmendoza@users.noreply.github.com>
2023-05-16 18:19:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
-if (prevScrollpos > currentScrollPos) {
+if (prevScrollpos > currentScrollPos || window.pageYOffset < 65) {
     document.getElementById("navbar").style.top = "0";
     document.getElementById("navbar").style.opacity = "1";
 } else {
